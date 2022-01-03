56305857e60174853eaee654898a1dd5ec9aa133
sp863 <sp863429@gmail.com>
2022-01-03 11:15:47 +0900

    carousel: arrow keys listener

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 "use strict";
 
 const slides = document.querySelectorAll(".slide");
-// 0% 100% 200% 300% 400%
 const btnLeft = document.querySelector(".btn-left");
 const btnRight = document.querySelector(".btn-right");
+const dotsContainer = document.querySelector(".dots");
 
 let curSlide = 0;
 const maxSlide = slides.length - 1;
@@ -11,6 +11,7 @@ const maxSlide = slides.length - 1;
 const moveSlide = function (slide) {
   slides.forEach(
     (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+    // 0% 100% 200% 300% 400%
   );
 };
 
@@ -38,3 +39,7 @@ const preSlide = function () {
 
 btnLeft.addEventListener("click", preSlide);
 btnRight.addEventListener("click", nextSlide);
+document.addEventListener("keydown", function (e) {
+  e.key === "ArrowLeft" && preSlide();
+  e.key === "ArrowRight" && nextSlide();
+});
