c3aee5ab614e919c57c897c7e9f11963385e517e
sp863 <sp863429@gmail.com>
2022-01-03 11:08:10 +0900

    carousel: move slide function and listener

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
 "use strict";
 
-const slides = document.querySelectorAll("slide");
+const slides = document.querySelectorAll(".slide");
+// 0% 100% 200% 300% 400%
+const btnLeft = document.querySelector(".btn-left");
+const btnRight = document.querySelector(".btn-right");
+
+let curSlide = 0;
+const maxSlide = slides.length - 1;
+
+const moveSlide = function (slide) {
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+  );
+};
+
+moveSlide(0);
+
+//Next Slide
+
+const nextSlide = function () {
+  if (curSlide === maxSlide) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+  moveSlide(curSlide);
+};
+
+const preSlide = function () {
+  if (curSlide === 0) {
+    curSlide = maxSlide;
+  } else {
+    curSlide--;
+  }
+  moveSlide(curSlide);
+};
+
+btnLeft.addEventListener("click", preSlide);
+btnRight.addEventListener("click", nextSlide);
